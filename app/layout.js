261420d3c361c07f033377c12c99b8cbe3520613
rakapanduwiradja-rpw <rakapanduwiradja@gmail.com>
2026-04-21 20:07:48 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
-        <link rel="icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'%3E%3Ctext y='26' font-size='26'%3E%F0%9F%8C%B9%3C/text%3E%3C/svg%3E" />
+        <link rel="icon" href="/Brandmark.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/Brandmark.png" />
       </head>
       <body>
         <Providers>
