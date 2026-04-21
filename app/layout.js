eea9825b653ce05407677c5f5da739647c045645
rakapanduwiradja-rpw <rakapanduwiradja@gmail.com>
2026-04-21 14:34:40 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -29,8 +29,9 @@ export default function RootLayout({ children }) {
           rel="icon"
           href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'%3E%3Ctext y='26' font-size='26'%3E%F0%9F%8C%B9%3C/text%3E%3C/svg%3E"
         />*/
+        {  
         <link rel="icon" href="/Brandmark.png" type="image/png" />
-
+          }
         {/*
           OPSI 2: Pakai file favicon sendiri
           1. Upload file favicon.ico atau favicon.png ke folder /public di GitHub
